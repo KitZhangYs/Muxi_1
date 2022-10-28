a9d9b2eefd53e04b703678c8a82154cacc5ac751
KitZhangYs <SJMbaiyang@163.com>
2022-10-28 20:16:44 +0800

    c50第十一题第一次提交，超时，待后续优化

---
+++ item11.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+	int people[100000];
+	int n,m;
+	while (scanf("%d%d",&n,&m)!=EOF){
+		if (n==0&&m==0)
+			break;
+		int i,j;
+		for (i=0;i<n;i++){
+			scanf("%d",&people[i]);
+		}
+		int end=n;
+		while (end){
+			for (i=0;i<n;i++){
+				if(people[i-1]<people[i]){
+					int tem = people[i];
+					people[i]=people[i-1];
+					people[i-1]=tem;
+				}
+			}
+			end--;
+		}
+		if (n>=m){
+			for(i=0;i<m;i++){
+				printf("%d ",people[i]);
+			}
+		}else{
+			for(i=0;i<n;i++){
+				printf("%d ",people[i]);
+			}
+		}
+		putchar('\n');
+	}
+}
